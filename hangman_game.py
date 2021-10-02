f8d0ad0a92f302b780f61f6c0c336a205f8c3fa9
Carlos Ernesto Salgado <salgadocarlosernesto11@gmail.com>
2021-10-01 23:37:17 -0400

    Hangman game Done!

--- hangman_game.py
+++ hangman_game.py
@@ -1,45 +1,46 @@
 import random, os
+import time
 
 # Lectura de la base de datos
 def read():
-    palabras = []
+    words = []
+    
     with open('./files/data.txt', 'r', encoding='UTF-8') as f:
         for line in f:
-            palabras.append(line)
-    return palabras
+            words.append(line.strip().upper())
+    return words
 
 
 
 def main():
-    index = random.randint(1, len(read())) # obtengo palabra random
-    word = read()[index]                   # guardo la palabra
-    spaces = '_'*(len(word)-1)             # creo los espacios para la palabra
-    word_index = dict(enumerate(word))     # utilizo la fi=uncion enumerate() que recibe como parametro un iterable
-    word_index.popitem()                   # elimino el ultimo par clave:valor del diccionario que es un  \n 
-    spaces = dict(enumerate(spaces))
+    data = read()
+    random_word = random.choice(data)
+    list_word = [letter for letter in random_word]
+    list_spaces = ["_"] * len(list_word)
+    dict_of_word = dict(enumerate(random_word))
+    
+    while list_spaces != list_word:
+        try:
+            os.system('cls')
+            print('Adivina la palabra!')
+            for element in list_spaces:
+                print(element + ' ', end='')
+            print('\n')
 
-    error = 0                               # Creo un contador 
-    good = 0                                # Contador
-    while word_index != spaces:
-        print(error)
-        char = input('Entre una letra: ')
-        assert not char.isnumeric(), 'Debe entrar solo letras'
-        for key, value in word_index.items():
-            if value == char:
-                spaces[key] = value
-                list = [value for value in spaces.values()]
-                act = " ".join(list)
+            char = input('Entre una letra: ').strip().upper()
+            if len(char) > 1 or not char.isalpha():
+                raise ValueError('Error!. En la entrada de Datos')
+            for key, value in dict_of_word.items():
+                if value == char:
+                    list_spaces[key] = value
+
+            if '_' not in list_spaces:
                 os.system('cls')
-                print('Adivine!')
-                print(act)
-                good += 1
-            else:
-                # No acert
-                error += 1
-        if word_index == spaces:
-            print(f'Adivinaste, la palabra es: {word}')
-            print(f'Total de entradas: {good+error}')
-            print(f'Errores:  {error}')
+                print(f'Felicidades ha ganado! \nLa palabra es {random_word}')
+        except ValueError as error:
+            os.system('cls')
+            print(error)
+            time.sleep(3)
             
 
 
